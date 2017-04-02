fbf635181b7caa33817afc2083e1aba6402a8a40
Sebastian Szachnowicz <libatoor@gmail.com>
2017-04-02 20:05:01 +0200

    Commit with working map activiti

--- app/src/main/java/com/pwr/projekt/enduroracepilot/MainActivity.java
+++ app/src/main/java/com/pwr/projekt/enduroracepilot/MainActivity.java
@@ -1,7 +1,11 @@
 package com.pwr.projekt.enduroracepilot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.pwr.projekt.enduroracepilot.activities.MapsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToMapActivity(View view) {
+        Intent mapActivity = new Intent(this, MapsActivity.class);
+        startActivity( mapActivity);
+
     }
 }
